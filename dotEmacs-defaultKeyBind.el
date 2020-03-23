4ed2ed3b50ee2d6cc58da262f0ff7285faf778b3
Alex Hon <alexhon@AlexMacBookPro.local>
2020-03-22 21:11:12 -0400

    Set the backup directory to .emacs.d/backup. Moved the .gitignore to the subdirs of dotEmacsDir

--- dotEmacs-defaultKeyBind.el
+++ dotEmacs-defaultKeyBind.el
@@ -107,6 +107,24 @@
 ;; ============================================
 (defalias 'list-buffers 'ibuffer)
 
+;; ============================================
+;; Centralized back up
+;; ============================================
+(if (file-directory-p "~/.emacs.d/backup")
+  (setq backup-directory-alist '(("." . "~/.emacs.d/backup")))
+  (message "Directory does not exist: ~/.emacs.d/backup"))
+
+(defvar user-temporary-file-directory
+  (concat temporary-file-directory user-login-name "/"))
+(make-directory user-temporary-file-directory t)
+(setq backup-by-copying t)
+(setq backup-directory-alist
+      `(("." . ,user-temporary-file-directory)
+        (,tramp-file-name-regexp nil)))
+(setq auto-save-list-file-prefix
+      (concat user-temporary-file-directory ".auto-saves-"))
+(setq auto-save-file-name-transforms
+      `((".*" ,user-temporary-file-directory t)))
 
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; EOF
